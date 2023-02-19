8e74c130090929dbecf1ecaaf34ab3440de436da
RomanBratus <romanbratus@gmail.com>
2023-02-19 13:30:22 +0200

    1

--- src/js/api.js
+++ src/js/api.js
@@ -1,10 +1,6 @@
-const ENDPOINT = 'https://pixabay.com/api/';
-
-
-
 function fetchData(query) {
   return fetch(
-    `${ENDPOINT}?key=33763502-a627660fe51c212db75711873&image_type=photo&orientation=horizontal&safesearch=true&q=${query}`
+    `https://pixabay.com/api?key=33763502-a627660fe51c212db75711873&image_type=photo&orientation=horizontal&safesearch=true&q=${query}`
   )
     .then(response => response.json())
 }
